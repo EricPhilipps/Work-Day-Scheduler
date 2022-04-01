8bd9ae2fd01247cbad3e07d063239b3ab7e3da69
Eric Philipps <ericphilipps@Erics-MacBook-Pro.local>
2022-03-31 23:32:28 -0500

    Unsure of how to proceed

--- Develop/script.js
+++ Develop/script.js
@@ -6,20 +6,20 @@ var hiddenBlocks = $('.hidden-blocks');
 
 //hiddenBlocks.hide();
 
-var div = timeBlocksContEl.append(div);
-var row = div.append(div);
-row.addClass("row col-12");
-var hourBlock = row.append(div);
-hourBlock.addClass("hour col-3");
-var tenseBlock = row.append(div);
-tenseBlock.addClass("past col-6");
-//var tense = tenseBlock.append(p);
-var btnBlock = row.append(div)
-btnBlock.addClass("saveBtn col-3");
-//var button = btnBlock.append(button);
+// var div = timeBlocksContEl.append(div);
+// var row = div.append(div);
+// row.addClass("row col-12");
+// var hourBlock = row.append(div);
+// hourBlock.addClass("hour col-3");
+// var tenseBlock = row.append(div);
+// tenseBlock.addClass("past col-6");
+// //var tense = tenseBlock.append(p);
+// var btnBlock = row.append(div)
+// btnBlock.addClass("saveBtn col-3");
+// //var button = btnBlock.append(button);
 
 
-function createBlocks() {
+function createTense() {
     var hour;
     
     for (var i = 9; i < 18; i++) {
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Work-Day-Scheduler
+
+I'm confused as to where to go from here. Going to need to get help in class.
